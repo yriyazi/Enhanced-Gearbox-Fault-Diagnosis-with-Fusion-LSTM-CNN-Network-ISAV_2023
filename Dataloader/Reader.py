f86d2408423066239ca03afa9b64787f95a5b529
yriyazi <iyasiniyasin98@gmail.com>
2023-09-11 08:47:54 +0330

    Datset Update

---
+++ Dataloader/Reader.py
@@ -0,0 +1,73 @@
+import os
+import csv
+import numpy as np
+import pandas as pd
+
+def read_csv_files(folder_path):
+    csv_files = []
+    for root, dirs, files in os.walk(folder_path):
+        for file in files:
+            if file.endswith(".csv"):
+                file_path = os.path.join(root, file)
+                with open(file_path, 'r', newline='') as csv_file:
+                    csv_reader = csv.reader(csv_file)
+                    data = list(csv_reader)
+                    csv_files.append((file_path, data))
+    return csv_files
+    
+def array_iefier(csv_files):
+    _set = {}
+    for i in range(10):
+        name = str(csv_files[i][0].split('\\')[-1].split('.')[0])
+        _set[name] = None
+
+        float_list = [] 
+        for _row in csv_files[i][1][1:]:
+            float_list .append([float(element) for element in _row])
+
+        _set[name] = np.array(float_list)
+    return _set
+
+def _unifier(_set,_min):
+    for i in _set.keys():
+        _set[i] = _set[i][:_min,:]
+    return _set
+
+def dataser_reader():
+    _min = 88320
+
+    csv_files_Faulty  = read_csv_files(r'Dataset\Gear Orginal\BrokenTooth')
+    csv_files_Healthy = read_csv_files(r'Dataset\Gear Orginal\Healthy')
+
+    _Faulty = array_iefier(csv_files_Faulty)
+    _Health = array_iefier(csv_files_Healthy)
+
+    _Faulty = _unifier(_Faulty,_min)
+    _Health = _unifier(_Health,_min)
+
+    label_F = []
+    for label in _Faulty.keys():
+        label_F.append(label)
+
+    label_H = []
+    for label in _Health.keys():
+        label_H.append(label)
+
+    columns = ['Data', 'Faulty']
+    df      = pd.DataFrame( columns=columns)
+
+    for index in range(0,10):
+        df_temp_F = pd.DataFrame( columns=columns)
+        df_temp_H = pd.DataFrame( columns=columns)
+
+        df_temp_F['Data']     = _Faulty[label_F[index]][:,0]
+        df_temp_F['Faulty']   = np.ones((_min))
+        df = pd.concat([df, df_temp_F], ignore_index=True)
+        del df_temp_F
+
+        df_temp_H['Data']     = _Health[label_H[index]][:,0]
+        df_temp_H['Faulty']   = np.zeros((_min))
+        df = pd.concat([df, df_temp_H], ignore_index=True)
+        del df_temp_H
+
+    return df
--- Dataloader/__init__.py
+++ Dataloader/__init__.py
@@ -1,53 +1,5 @@
-# import pandas as pd
-# import Utils
-
-# df = pd.read_csv('E:\Thesis\ISAV2023V2\ISAV_2023_V2\Dataset\Dataset-Raja.csv')
-# #df['Faulty'] = df['Faulty'].replace({False: 0, True: 1})
-
-# """
-# print(df.head())
-
-# its better to keep numpy format because CWT uses numpy and before network make it pytorch
-
-# X = torch.tensor(df['Datas' ].values).reshape(Total_test,samples_per_test)
-# Y = torch.tensor(df['Faulty'].values).reshape(Total_test,samples_per_test)
-# """
-
-# """
-
-# """
-
-# mask = df['Test_ID'] != 2 
-# df_filt = df[mask]
-
-# mask2 = df['Test_ID'] != 10
-# df_filtered = df_filt[mask2]
-
-# df_filtered['Test_ID'].unique()
-
-# """
-
-# Test 2 & test 10 are removed
-
-# """
-# df_loaded   = df_filtered[df_filtered['Generator']==1]
-# df_filtered = df_filtered[df_filtered['Generator']!=1]
-# df_filtered = df_filtered[df_filtered['C6']!=1]
-
-# df_filtered['label'] = df_filtered['C5'].apply(lambda x: 1 if x==1 else 0)
-
-
-# data = df_filtered.drop(columns=['Column1', 'IDs', 'Time', 'Name_file', 'C5', 'C6', 'Manifold', 'Generator'])
-# data.head()
-
-
-# Utils.Total_test = data['Datas' ].values.shape[0]//Utils.samples_per_test
-# X_numpy = data['Datas' ].values.reshape(Utils.Total_test,Utils.samples_per_test)
-# Test_ID_numpy = data['Test_ID'].values.reshape(Utils.Total_test,Utils.samples_per_test)
-# Y_numpy = data['label'].values.reshape(Utils.Total_test,Utils.samples_per_test)
-
-import pandas as pd
-df = pd.read_csv('Dataset\Gear.csv')
+from .Reader import dataser_reader
+df = dataser_reader()
 
 _datas = 20
 X_numpy = df['Data' ].values.reshape(_datas,len(df['Data'])//_datas)
